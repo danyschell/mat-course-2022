ac5ef6ade67fa7ed7461724a16db874fde919316
Daniel Schell <schell.daniel@gmail.com>
2024-04-13 21:58:36 -0300

    routing

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// import { LoginComponent } from './auth/login/login.component';
+// import { BlogPageComponent } from './blogs/blog-page/blog-page.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+  ]
+})
+export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
+
+
 import { LoginComponent } from './auth/login/login.component';
+import { BlogPageComponent } from './blogs/blog-page/blog-page.component';
 
 export const routes: Routes = [
-    { path: "login", component: LoginComponent }
+    // { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: "login", component: LoginComponent },
+    { path: "blogPage", component: BlogPageComponent },
+    // { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
